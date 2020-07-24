66bcb5abd313e2026ee9b884d2366c89d536bb48
Vinicius Canuto <vcanutodev@gmail.com>
2020-07-23 23:06:10 -0300

    Resolução do desafio

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 
-import filesize from 'filesize';
+// import filesize from 'filesize';
 
 import Header from '../../components/Header';
 import FileList from '../../components/FileList';
@@ -20,22 +20,26 @@ interface FileProps {
 
 const Import: React.FC = () => {
   const [uploadedFiles, setUploadedFiles] = useState<FileProps[]>([]);
-  const history = useHistory();
+  // const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
+    const data = new FormData();
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post<FileProps[]>('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    setUploadedFiles([
+      {
+        name: files[0].name,
+        file: files[0],
+        readableSize: String(files[0].size),
+      },
+    ]);
   }
 
   return (
